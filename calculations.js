d107ca738183e05e54cacc8e306f5dd3eb5976bc
elena-rubakha-nyc <136662440+elena-rubakha-nyc@users.noreply.github.com>
2023-07-09 00:52:10 -0400

    Homework 6.1: fixed calculations.js code (see comments)

--- calculations.js
+++ calculations.js
@@ -3,15 +3,19 @@ function increaseBalance(x, y) {
   return x;
 }
 
-function decreaseBalance(x) {
-x = x - y;
-return x;
+function decreaseBalance(x, y) { //added missing y argument
+  x = x - y;
+  return x;
 }
 
 function divideBalanceByAccounts(x, y) {
   x = x / y;
   return x;
-  console.log("x");
+  // console.log("x"); 
+  /* as a return statement terminates the execution of a function 
+  if user wishes to execute this command, 
+  it needs to be moved before the return statement. I suppose this statement is just a mistake
+  as printing "x" string does not make any sense */
 }
 
 function getRestAfterDivision(x, y) {
@@ -21,5 +25,7 @@ function getRestAfterDivision(x, y) {
 
 console.log(increaseBalance(3000, 700));
 console.log(decreaseBalance(3000, 700));
-console.log(divideBalanceByAccounts(3000, 2, 4000));
-console.log(getRestAfterDivision(7000, 3));
+console.log(divideBalanceByAccounts(3000, 2)); /* Incorrect number of arguments:
+the function is called with three arguments (3000, 2, 4000), but expects 2. 
+Removed the extra argument to match the function's parameter count.*/ 
+console.log(getRestAfterDivision(7000, 3)); 
